c1974bbfb32681e8abd184c69a5fa0258e460a23
minhmen93 <119550691+minhmen93@users.noreply.github.com>
2023-03-23 01:53:17 +0700

    Add files via upload

---
+++ MD3_Bai11_ThucHanh_DSA Stack_Queue/src/thucHanh1_Stack_Trong_LinkedList/GenericStackClient.java
@@ -0,0 +1,15 @@
+package thucHanh1_Stack_Trong_LinkedList;
+
+import static thucHanh1_Stack_Trong_LinkedList.MyGenericStack.stackOfIStrings;
+import static thucHanh1_Stack_Trong_LinkedList.MyGenericStack.stackOfIntegers;
+
+public class GenericStackClient {
+    public static void main(String[] args) {
+                System.out.println("1. Stack of integers");
+                stackOfIntegers();
+                System.out.println("\n2. Stack of Strings");
+                stackOfIStrings();
+            }
+        }
+
+
---
+++ MD3_Bai11_ThucHanh_DSA Stack_Queue/src/thucHanh4_Stack_SD_mang/StackClient.java
@@ -0,0 +1,25 @@
+package thucHanh4_Stack_SD_mang;
+
+public class StackClient {
+    public static void main(String[] args) throws Exception {
+
+                Stack_Arr stack = new Stack_Arr(5);
+
+                stack.push(5);
+                stack.push(4);
+                stack.push(3);
+                stack.push(2);
+                stack.push(1);
+
+                System.out.println("1. Size of stack after push operations: " + stack.size());
+                System.out.printf("2. Pop elements from stack : ");
+
+                while (!stack.isEmpty()) {
+                    System.out.printf(" %d", stack.pop());
+                }
+
+                System.out.println("\n3. Size of stack after pop operations : " + stack.size());
+            }
+        }
+
+ 
